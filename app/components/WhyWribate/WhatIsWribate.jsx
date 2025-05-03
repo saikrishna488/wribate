c363edb0ac082cd288ba788844e5f08fa73b3e9e
saikrishna488 <oltvez@gmail.com>
2025-05-03 13:26:12 +0530

    new commit

--- app/components/WhyWribate/WhatIsWribate.jsx
+++ app/components/WhyWribate/WhatIsWribate.jsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/navigation';
 import React from 'react';
 
 
-export default function WhatIsWribateSection() {
+export default function WhatIsWribate() {
 
   const router = useRouter();
   return (
