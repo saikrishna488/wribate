33247519a0282be4d293ac92e18c21304d66f3c8
saikrishna488 <oltvez@gmail.com>
2025-05-13 00:47:17 +0530

    13/4/25

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com'],
-      },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
